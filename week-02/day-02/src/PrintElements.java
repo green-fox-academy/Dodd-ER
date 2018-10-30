668776e54f4d5345509130de9fd6d4d2a229c31c
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-10-30 16:06:04 +0100

    PrintElements

---
+++ week-02/day-02/src/PrintElements.java
@@ -0,0 +1,12 @@
+public class PrintElements {
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `numbers`
+
+    public static void main(String[] args){
+        int numbers[] = {4, 5, 6, 7};
+        for (int elements : numbers){
+            System.out.println(elements);
+        }
+    }
+}
