7b2341159bfa7d7d6e1f6a5d8f530a4b16fe294f
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-10-30 17:46:42 +0100

    Reverse_NotFinished

---
+++ week-02/day-02/src/Reverse.java
@@ -0,0 +1,18 @@
+public class Reverse {
+    // - Create an array variable named `aj`
+    //   with the following content: `[3, 4, 5, 6, 7]`
+    // - Reverse the order of the elements in `aj`
+    // - Print the elements of the reversed `aj`
+
+    public static void main(String[] args) {
+        int[] aj = {3, 4, 5, 6, 7};
+
+        //   for (int i = 4; i >= 0; i--){
+        //        System.out.print("|" + aj[i] + "|");
+
+        for (int i = 0; i < aj.length; i++){
+            
+        }
+    }
+}
+
