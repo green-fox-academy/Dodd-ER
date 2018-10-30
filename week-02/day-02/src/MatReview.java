bd419f0828584f779de19697c38eecb8d624c709
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-10-30 12:22:00 +0100

    MatRev&Doubling

---
+++ week-02/day-02/src/MatReview.java
@@ -0,0 +1,11 @@
+public class MatReview {
+
+    public static final int MY_NUMBER = 5;
+    // Uppercase + aláhúzás a constansoknál
+
+    public static void main(String[] args){
+
+        System.out.println(MY_NUMBER);
+
+    }
+}
