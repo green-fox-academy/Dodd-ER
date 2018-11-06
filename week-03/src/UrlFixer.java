6a3479f3ca7aa28427c0d2d17ca9053c1daead1d
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-06 13:35:24 +0100

    TakesLonger

--- week-03/src/UrlFixer.java
+++ week-03/src/UrlFixer.java
@@ -5,7 +5,7 @@ public class UrlFixer {
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
         // Also, the URL is missing a crutial component, find out what it is and insert it too!
         // Try to solve it more than once using different String functions!
-        
+
         url = url.replace("bots", "odds");
         String url2 = url.substring(5);
         url = url.substring(0, 4);
