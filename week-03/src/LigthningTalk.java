7a4ff7f72ebc2c984f7926d490adc2022c12359b
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-06 10:34:58 +0100

    LightningTalk

---
+++ week-03/src/LigthningTalk.java
@@ -0,0 +1,16 @@
+public class LigthningTalk {
+
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        int num = 12;
+        int num1 = 14;
+        int num2 = 13;
+
+
+    }
+}
