3aba74d27ccca3de56e0bc7268b071d841e6c3a3
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-07 13:58:07 +0100

    CenterBoxFunction

---
+++ week-03/day-03/src/CenterBoxFunction.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterBoxFunction {
+
+    public static void mainDraw(Graphics graphics){
+        // create a square drawing function that takes 1 parameter:
+        // the square size
+        // and draws a square of that size to the center of the canvas.
+        // draw 3 squares with that function.
+        // avoid code duplication.
+
+        for (int i = 0; i < 3; i++){
+            int randomNum = (int)(Math.random() * WIDTH);
+            SquareDrawingFunction(randomNum, graphics);
+        }
+    }
+
+    public  static void SquareDrawingFunction(int num1, Graphics graphics){
+        graphics.setColor(new Color((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256)));
+        graphics.drawRect((WIDTH - num1) / 2, (HEIGHT - num1) / 2, num1, num1);
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
