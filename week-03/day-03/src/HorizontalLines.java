5d1e2781e29bb887d71ef612999251be1969daff
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-07 12:34:11 +0100

    HorizontalLines

---
+++ week-03/day-03/src/HorizontalLines.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 2 parameters:
+        // The x and y coordinates of the line's starting point
+        // and draws a 50 long horizontal line from that point.
+        // Draw 3 lines with that function. Use loop for that.
+
+        for (int i = 0; i < 3; i++){
+            int xCoord = (int)(Math.random() * WIDTH);
+            int yCoord = (int)(Math.random() * HEIGHT);
+            DrawingFunction(xCoord, yCoord, graphics);
+        }
+
+    }
+
+    public static void DrawingFunction(int num1, int num2, Graphics graphics){
+        graphics.drawLine(num1, num2, (num1 + 50), num2);
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
