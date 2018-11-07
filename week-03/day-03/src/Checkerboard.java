40fc4d8aab47d86c031fb69823c6a8461ea596d7
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-07 16:37:43 +0100

    Checkerboard

---
+++ week-03/day-03/src/Checkerboard.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class Checkerboard {
+    public static void mainDraw(Graphics graphics) {
+        // Fill the canvas with a checkerboard pattern.
+        for (int i = 40; i < 320; i+= 80){
+            for (int j = 0; j < 320; j += 80){
+                graphics.fillRect(j,i - 40,40,40);
+            }
+            for (int k = 40; k < 320; k += 80){
+                graphics.fillRect(k,i,40,40);
+            }
+        }
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
