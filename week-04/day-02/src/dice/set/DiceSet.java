e870c34804a72c0b3dd67053307071216908fbeb
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-13 16:57:24 +0100

    DiceSet

---
+++ week-04/day-02/src/dice/set/DiceSet.java
@@ -0,0 +1,59 @@
+package dice.set;
+
+import java.util.Arrays;
+
+public class DiceSet {
+    // You have a `DiceSet` class which has a list for 6 dices
+// You can roll all of them with roll()
+// Check the current rolled numbers with getCurrent()
+// You can reroll with reroll()
+// Your task is to roll the dices until all of the dices are 6
+    int[] dices = new int[6];
+
+    public int[] roll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+        return dices;
+    }
+
+    public int[] getCurrent() {
+        return dices;
+    }
+
+    public int getCurrent(int i) {
+        return dices[i];
+    }
+
+    public void reroll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+    }
+
+    public void reroll(int k) {
+        dices[k] = (int) (Math.random() * 6) + 1;
+    }
+
+    public static void main(String[] args) {
+        DiceSet diceSet = new DiceSet();
+        diceSet.roll();
+        int numOfSixes = 0;
+        int howManyTimes = 0;
+        do {
+            for (int i = 5; i >= 0; i--){
+                if (diceSet.getCurrent(i) == 6){
+                    System.out.println(diceSet.getCurrent(i));
+                    if (diceSet.getCurrent(i) > 1) {
+                        diceSet.reroll(i - 1);
+                    }
+                    numOfSixes++;
+                }
+                howManyTimes++;
+            }
+        } while (numOfSixes < 6);
+        System.out.println("Interested, how many times I need to roll a dice? That many: " + howManyTimes);
+
+
+    }
+}
