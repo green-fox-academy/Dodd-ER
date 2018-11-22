262fa88660872cf42955f6ce17f71aa6e5840afe
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-11-22 12:12:05 +0100

    SumOfNumsWithNull

--- week-05/day-04/src/sum/ListOFNumbers.java
+++ week-05/day-04/src/sum/ListOFNumbers.java
@@ -8,18 +8,22 @@ public class ListOFNumbers {
 
   public ListOFNumbers() {
     this.nums = new ArrayList<>();
+    nums.add(null);
   }
 
   public ListOFNumbers(int numberOfElements) {
     this.nums = new ArrayList<>();
 
-    for (int i = 1; i <= numberOfElements; i++) {
+    for (int i = 0; i <= numberOfElements; i++) {
       nums.add(i);
     }
   }
 
-  public int sum() {
+  public Object sum() {
     int sumOfNums = 0;
+    if (this.nums.get(0) == null) {
+      return null;
+    } else
     for (int elements : this.nums) {
       sumOfNums += elements;
     }
--- week-05/day-04/src/sum/testListOfNums.java
+++ week-05/day-04/src/sum/testListOfNums.java
@@ -6,20 +6,26 @@ import org.junit.jupiter.api.Test;
 
 public class testListOfNums {
 
-  @Test
+  @Test ()
   public void testSum1() {
     ListOFNumbers newNums = new ListOFNumbers();
-    assertEquals(0, newNums.sum());
+    assertEquals(null, newNums.sum());
   }
 
   @Test
   public void testSum2() {
+    ListOFNumbers newNums = new ListOFNumbers(0);
+    assertEquals(0, newNums.sum());
+  }
+
+  @Test
+  public void testSum3() {
     ListOFNumbers newNums = new ListOFNumbers(1);
     assertEquals(1, newNums.sum());
   }
 
   @Test
-  public void testSum3() {
+  public void testSum4() {
     ListOFNumbers newNums = new ListOFNumbers(3);
     assertEquals(6, newNums.sum());
   }
