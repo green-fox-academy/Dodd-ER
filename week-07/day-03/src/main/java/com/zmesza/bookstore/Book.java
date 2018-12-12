0c8f062244b0a34b39eae460b7aefa538d3ea8f1
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-12-12 12:10:50 +0100

    Books

---
+++ week-07/day-03/src/main/java/com/zmesza/bookstore/Book.java
@@ -0,0 +1,33 @@
+package com.zmesza.bookstore;
+
+public class Book {
+  private static int nextId = 0;
+
+  private int id;
+  private String title;
+  private String author;
+  private Integer releaseYear;
+
+  public Book(String title, String author, Integer releaseYear) {
+    this.id = nextId++;
+    this.title = title;
+    this.author = author;
+    this.releaseYear = releaseYear;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
+
+  public Integer getReleaseYear() {
+    return this.releaseYear;
+  }
+}
