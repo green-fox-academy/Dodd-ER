b9255dda1184f05ec47749c15596f509667f1728
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-12-13 12:32:52 +0100

    HelloBeanWorld

---
+++ week-07/day-04/hellobeanworld/src/main/java/com/zmesza/hellobeanworld/HellobeanworldApplication.java
@@ -0,0 +1,23 @@
+package com.zmesza.hellobeanworld;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobeanworldApplication implements CommandLineRunner {
+
+  @Autowired
+  Printer pnt;
+
+  public static void main(String[] args) {
+    SpringApplication.run(HellobeanworldApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    pnt.log("hello");
+  }
+}
+
