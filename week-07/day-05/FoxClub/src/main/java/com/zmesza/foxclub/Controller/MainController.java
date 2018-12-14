7c5a830c7c848068e40ef2ecd5d8aeb0284cfad2
Mészáros Zoltán <m.zoltan17@gmail.com>
2018-12-14 13:47:12 +0100

    ReceivingFoxName

--- week-07/day-05/FoxClub/src/main/java/com/zmesza/foxclub/Controller/MainController.java
+++ week-07/day-05/FoxClub/src/main/java/com/zmesza/foxclub/Controller/MainController.java
@@ -1,24 +1,42 @@
 package com.zmesza.foxclub.Controller;
 
+import com.zmesza.foxclub.Service.Fox;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-  @RequestMapping("/info")
-  public String index() {
+  private Fox myFox;
+
+  @Autowired
+  public MainController(Fox myFox) {
+    this.myFox = myFox;
+  }
+
+  @GetMapping("/info")
+  public String index(Model model) {
+    model.addAttribute("name", myFox.getName());
+    model.addAttribute("food", myFox.getFood());
+    model.addAttribute("drink", myFox.getDrink());
     return "information";
   }
 
-  @RequestMapping("/login")
+  @GetMapping("/login")
   public String loginGet() {
     return "login";
   }
 
-  @PostMapping()
-  public String loginPost() {
+  @PostMapping("/info")
+  public String loginPost(@RequestParam String nameInput, Model model) {
+    myFox.setName(nameInput);
+    model.addAttribute("name", myFox.getName());
+    model.addAttribute("food", myFox.getFood());
+    model.addAttribute("drink", myFox.getDrink());
     return "information";
   }
 }
--- week-07/day-05/FoxClub/src/main/java/com/zmesza/foxclub/Service/Fox.java
+++ week-07/day-05/FoxClub/src/main/java/com/zmesza/foxclub/Service/Fox.java
@@ -1,9 +1,12 @@
 package com.zmesza.foxclub.Service;
 
+import org.springframework.stereotype.Service;
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class Fox {
   private int id;
   private String name;
@@ -11,12 +14,43 @@ public class Fox {
   private String drink;
   private List<String> listOfTricks;
 
-  public Fox(String name) {
+  public Fox() {
     this.id = 1;
-    this.name = name;
+    this.name = "Mr.Fox";
     this.food = "Pasta";
     this.drink = "Tonic";
     this.listOfTricks = Arrays.asList("Write HTML","Code in Java","Dumb in CSS");
   }
 
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFood() {
+    return food;
+  }
+
+  public void setFood(String food) {
+    this.food = food;
+  }
+
+  public String getDrink() {
+    return drink;
+  }
+
+  public void setDrink(String drink) {
+    this.drink = drink;
+  }
+
+  public List<String> getListOfTricks() {
+    return listOfTricks;
+  }
 }
